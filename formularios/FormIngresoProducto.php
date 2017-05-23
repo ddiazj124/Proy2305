d744de112b6243df6243352c6f76cf97b7b1a23b
Diego <cetecom@LC08L5703>
2017-05-23 11:27:26 -0400

    Wena m3n!

--- formularios/FormIngresoProducto.php
+++ formularios/FormIngresoProducto.php
@@ -4,8 +4,11 @@
         <title></title>
     </head>
     <body>
-        <form method="POST" action="">
-            
+        <form method="POST" action="../controladores/IngresoProducto.php">
+            <div><label>Nombre</label><input type="text" name="nombreProducto"></div>
+            <div><label>Total USD</label><input type="number" name="totalProducto"></div>
+            <div><label>Año </label><input type="number" name="anioProducto"></div>
+            <input type="submit" value="Ingresar Datos">
         </form>
     </body>
 </html>
--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
-            
-        ?>
+    
+?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-        <?php
-            
-        ?>
+        <a href="formularios/FormIngresoProducto.php">Ingresar Producto</a><br>
+        <a href="formularios/FormActualizarClave.php">Cambiar Clave</a>
     </body>
 </html>
